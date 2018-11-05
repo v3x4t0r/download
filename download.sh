6cf7266a3af49394556eb2c54b5f8ec018a2453b
v3x4t0r <33897075+v3x4t0r@users.noreply.github.com>
2018-11-05 23:39:52 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,68 +1,171 @@
-h="help"
-if [ "$1" = "$h" ]
+#/bin/bash
+
+#Links.txt = All bookmark url`s
+#url.txt = All youtube videos from links.txt
+#file.txt = All files that are present
+#newsongs.txt = All songs that does not exist
+#code = urlcode for local files
+#urlcode = urlcode for youtube bookmarks
+#newurlcode = codes that are not present on the system
+if [[ $1 == "install" ]]
 then
-  echo "Mp3 downloader help"
-  echo "A script to make it simpler to download mp3 files"
-  echo ""
-  echo "1.option is to download a youtube playlist"
-  echo "2.option is to paste a link to the site with the video/mp3"
-  echo "3.option is to search, this function does not work everytime"
-  echo "4.option is to paste a file"
-  echo ""
-  echo "To use the file option (nr 4)"
-  echo "You need to have a .txt file with URL's(1 url pr line)"
-  echo ""
-  echo "Use Ctrl+C to end script"
-  echo ""
-  echo "You have to install youtube-dl - repositories will respect copywrites"
-  echo "sudo curl -L https://yt-dl.org/latest/youtube-dl -o /usr/local/bin/youtube-dl"
-  echo "sudo chmod a+rx /usr/local/bin/youtube-dl "
-  echo ""
-  echo "If you get this error: ffprobe or avprobe not found. Please install one."
-  echo "Install libav-tools (ffmpeg is needed to convert audio)"
-  exit
+  #Unlock dpkg if locked
+  if [ -f /var/lib/dpkg/lock ]
+  then
+    rm /var/lib/dpkg/lock
+  fi
+
+  #Check if build-essentials is installed
+  build=$(dpkg -l | grep build-essential)
+  if [ -z "$build" ]
+  then
+    echo "Build-Essentail Not installed, install it? y/n"
+    read install
+    var=y
+    if [ $install = $var ]
+    then
+      sudo apt install build-essential
+    else exit
+    fi
+    else
+      echo "Build-Essentail is installed"
+  fi
+
+  #Check if liblz4-dev is installed
+  lz4=$(dpkg -l | grep liblz4-dev)
+  if [ -z "$lz4" ]
+  then
+    echo "liblz4-dev = Not installed, install it? y/n"
+    read install1
+    var=y
+    if [ $install1 = $var ]
+    then
+      sudo apt install liblz4-dev
+    else exit
+    fi
+    else
+      echo "liblz4-dev is installed"
+  fi
+
+  #Check if pkg-config is installed
+  pkg=$(dpkg -l | grep pkg-config)
+  if [ -z "$pkg" ]
+  then
+    echo "pkg-config = Not installed, install it? y/n"
+    read install2
+    var=y
+    if [ $install2 = $var ]
+    then
+      sudo apt install pkg-config
+    else exit
+    fi
+    else
+      echo "liblz4-dev is installed"
+  fi
+
+  #Find path of lz4jsoncat if compiled
+  lz4jsoncat=$(sudo find / -name 'lz4jsoncat' 2> /dev/null)
+  #If lz4jsoncat does not exist, clone & compile
+  if [ -z "$lz4jsoncat" ]
+    then
+      echo "lz4jsoncat is not installed, install now? y/n"
+      read install3
+      if [ $install3 = 'y' ]
+      then
+        if [ -d "lz4json" ]
+        then
+          cd lz4json && make && cd
+        else
+          mkdir lz4json && cd lz4json
+          git clone https://github.com/andikleen/lz4json.git
+          cd lz4json && make && cd
+        fi
+      fi
+    else
+      echo "lz4jsoncat is installed @ $lz4jsoncat"
+    fi
+
+    youtubedl=$(sudo find / -name 'lz4jsoncat' 2> /dev/null)
+    if [ -z $youtubedl ]
+    then
+      echo "youtube-dl = Not installed, install it? y/n"
+      read install4
+      var=y
+      if [ $install4 = $var ]
+      then
+        sudo curl -L https://yt-dl.org/latest/youtube-dl -o /usr/local/bin/youtube-dl
+        sudo chmod a+rx /usr/local/bin/youtube-dl
+      else exit
+      fi
+      else
+        echo "Youtube-dl is installed"
+    fi
 fi
 
-echo -e " \e[31m1.Playlist\e[0m"
-echo -e " \e[33m2.link\e[0m"
-echo -e " \e[32m3.Search\e[0m"
-echo -e " \e[31m4.File\e[0m"
-echo " "
-echo "Enter a number: "
-read valg
 
-if [ "$valg" == "1" ]
-then
-    echo -e "\e[31mPaste the link to your playlist: (Youtube/Soundcloud)\e[0m"
-    read link
-    youtube-dl -cit --extract-audio --audio-format mp3 $link
-fi
-if [ "$valg" == "2" ]
-then
-  echo -e "\e[33mPaste your link here:\e[0m "
-  read link2
-  youtube-dl --extract-audio --audio-format mp3 -l $link2
-fi
-if [ "$valg" == "3" ]
-then
-  echo -e "\e[32mSearch:\e[0m"
-  read link3
-  youtube-dl -x --audio-format mp3 "gvsearch1:$link3"
-fi
-if [ "$valg" == "4" ]
-then
-  echo -e "\e[31mEnter the filename:\e[0m"
-  read file
-  while read -r line
+#find path of lz4jsoncat
+lz4jsoncat=$(sudo find / -name 'lz4jsoncat' 2> /dev/null)
+
+#decompress the jsonlz4 bookmarks
+bookmarks=$(sudo $lz4jsoncat ~/.mozilla/firefox/*.default/bookmarkbackups/*.jsonlz4)
+#Regex to match only url`s
+links=$(echo $bookmarks | grep -ohP '"uri":"[^"]*' | grep -ohP 'http?.*')
+
+#For loop to add each url to file
+> links.txt
+for line in $links
   do
-    link4="$line"
-    youtube-dl --extract-audio --audio-format mp3 -l $link4
-  done <"$file"
-else
-  exit
-fi
+    echo $line >> links.txt
+done
+
+#Remove duplicates and add youtube links from firefox to url.txt
+links=$(sort links.txt | uniq | grep watch?v=)
+IFS=$'\n'
+echo $links | sed -E -e 's/[[:blank:]]+/\n/g' > url.txt
+> newsongs.txt
+for x in $links
+do
+  end=$(echo $x | grep -ohP '==?.*' | sed  's/=/-/g')
+  #Find the title of the file
+  title=$(curl -s $x | grep -ohP '"title":"[^"]*' | awk '{print substr($0,10); }')
+  #Create full name of file
+  filename=$title$end".mp3"
+  echo $filename >> newsongs.txt
+  echo $end | cut -c 2- | sed 's/\s//g' >> urlcode0.txt
+  #Find existing files in directory (exclude .sh and .txt files)
+done
+sort urlcode0.txt > urlcode.txt
+rm urlcode0.txt
+
+files=$(find . -maxdepth 1 -type f -name '*.mp3' | sed 's#.*/##')
+
+echo $files | sed -E -e 's/.mp3/\n/g' > file.txt
+cat file.txt | awk '{print $NF}' | cut -d'-' -f2,3 | sed 's/\s//g' > code0.txt
+sort code0.txt > code.txt
+rm code0.txt
+
+diff code.txt urlcode.txt | grep '^>' | sed 's/^>\ //g' > newurlcode.txt
+>final.txt
+#grep for urlcode in urlcode.txt for each line in newurlcode.txt
+newurlcode=`cat newurlcode.txt`
+for z in $newurlcode
+do
+  cat url.txt | grep $z >> final.txt
+done
+
+links=`cat final.txt`
+
+for line in $links
+do
+  youtube-dl --extract-audio --audio-format mp3 -l $line 2> /dev/null
+done
 
 
-
-echo -e "Your download has been saved in: "
-\pwd
+#What files can be made to variables or removed??
+rm newsongs.txt
+rm newurlcode.txt
+rm urlcode.txt
+rm code.txt
+rm file.txt
+rm links.txt
+rm url.txt
